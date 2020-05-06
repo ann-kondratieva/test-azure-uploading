f2a00b88f102ddd30e3f2ca610754620eb58d7b8
Hanna Kandratsyeva <hanna.kandratsyeva@softensity.com>
2020-05-06 10:34:37 +0300

    Updates

---
+++ precache-manifest.8db52cbe38654c081c131a9974f0a253.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6be1301bdfe3444fd73df475b75e3c",
+    "url": "/test-azure-uploading/index.html"
+  },
+  {
+    "revision": "b9fdf7f60ede3fb360a5",
+    "url": "/test-azure-uploading/static/js/2.76a944d8.chunk.js"
+  },
+  {
+    "revision": "5275532c47227c8532f67cfc83c59424",
+    "url": "/test-azure-uploading/static/js/2.76a944d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abd9e8b30446d660e82",
+    "url": "/test-azure-uploading/static/js/main.fbc11224.chunk.js"
+  },
+  {
+    "revision": "bb4bcf066e65c4821ba2",
+    "url": "/test-azure-uploading/static/js/runtime-main.6c7d7fc9.js"
+  }
+]);
